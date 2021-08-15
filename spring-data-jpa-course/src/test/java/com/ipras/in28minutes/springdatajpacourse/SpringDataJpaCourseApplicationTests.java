4c6a9b83acd10699e1ceca5d01a926f1f8d3564f
iprashantgit <iprashantsharma@outlook.com>
2021-08-15 22:07:02 +0530

    spring boot rest bootstrap starter

---
+++ spring-data-jpa-course/src/test/java/com/ipras/in28minutes/springdatajpacourse/SpringDataJpaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipras.in28minutes.springdatajpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
